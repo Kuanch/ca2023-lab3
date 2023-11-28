fc17170207f4463ac2762dc572d4fe0c05b0f1d2
Kuanch <sixigma6@gmail.com>
2023-11-28 15:31:06 +0800

    add Fmul32Test

--- src/test/scala/riscv/singlecycle/CPUTest.scala
+++ src/test/scala/riscv/singlecycle/CPUTest.scala
@@ -113,3 +113,26 @@ class ByteAccessTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 }
+
+class Fmul32Test extends AnyFlatSpec with ChiselScalatestTester {
+  behavior.of("Single Cycle CPU")
+  it should "multiply 32-bits floating" in {
+    test(new TestTopModule("fmul32.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
+      for (i <- 1 to 50) {
+        c.clock.step(1000)
+        c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
+      }
+      c.io.mem_debug_read_address.poke(4.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect("h4083ffff".U)
+
+      c.io.mem_debug_read_address.poke(8.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(0.U)
+
+      c.io.mem_debug_read_address.poke(12.U)
+      c.clock.step(2)
+      c.io.mem_debug_read_data.expect("h40b51e4b".U)
+    }
+  }
+}
